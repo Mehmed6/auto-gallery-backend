6f4e8b7026f1d8e23c2b1ba7dd73dac6845bea22
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-30 04:46:30 +0100

    Add JWT authentication filter implementation
    Introduces JWTAuthenticationFilter to handle JWT-based authentication in HTTP requests. The filter validates tokens, sets authentication in the security context, and handles expired or invalid tokens with custom exceptions.

---
+++ src/main/java/com/doganmehmet/app/jwt/JWTAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.doganmehmet.app.jwt;
+
+import com.doganmehmet.app.exception.BaseException;
+import com.doganmehmet.app.exception.ErrorMessage;
+import com.doganmehmet.app.exception.MessageType;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JWTService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException
+    {
+        var header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        var token = header.substring(7);
+
+        try {
+            var username = jwtService.getUsernameByToken(token);
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                var userDetails = userDetailsService.loadUserByUsername(username);
+
+                if (jwtService.isTokenValid(token, userDetails)) {
+                    var authToken = new UsernamePasswordAuthenticationToken(userDetails, null,
+                            userDetails.getAuthorities());
+
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+        } catch (ExpiredJwtException ex) {
+            throw new BaseException(new ErrorMessage(MessageType.TOKEN_IS_EXPIRED, ex.getMessage()));
+        } catch (Exception ex) {
+            throw new BaseException(new ErrorMessage(MessageType.GENERAL_ERROR, ex.getMessage()));
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
